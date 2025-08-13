598c1cb3a6a6f74cabbbcc65db713e5b493f38f3
RafaFontoura <rafaelcosta12597@gmail.com>
2025-08-12 23:01:51 -0300

    Commit Inicial

---
+++ game01/game01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime;
+
+namespace MeuProjeto
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool tipo = true;
+            Random rnd = new Random();
+
+            int numero1 = rnd.Next(20);
+
+            while (tipo)
+            {
+                Console.WriteLine("Adivinhe o número entre 0 e 20: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num == numero1)
+                {
+                    Console.WriteLine("Parabéns, você acertou!");
+                    tipo = false;
+                }
+                else
+                {
+                    if (num != numero1)
+                    {
+                        if (num < numero1)
+                        {
+                            Console.WriteLine("O número é maior.");
+                        }
+                        else if (num > numero1)
+                        {
+                            Console.WriteLine("O número é menor.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Por favor, insira um número válido.");
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
